238e4d284ff5ac84d7fb6caa73bd4201170ffa73
Tiphaine H <t.helmer@pi2r.net>
2026-09-14 01:33:56 +0200

    clean code for train_baseline.py

--- models/train_baseline.py
+++ models/train_baseline.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 
 from featurizers.fingerprints import *
+
 
 def prepare_features(csv_path):
     df = pd.read_csv(csv_path)
@@ -14,27 +14,33 @@ def prepare_features(csv_path):
     y = df["solubility"].values
     return X, y
 
-X_train, y_train = prepare_features("data/esol_train.csv")
-X_test, y_test = prepare_features("data/esol_test.csv")
-
-# # fixing random_state to get reproducible results to know if we're improving
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # REGRESSION RANDOM FOREST:
-regr = RandomForestRegressor(n_estimators=200, random_state=0)
-regr.fit(X_train, y_train)
+def random_forest(X_train, y_train):
+    model = RandomForestRegressor(n_estimators=200, random_state=0)
+    model.fit(X_train, y_train)
+    return model
 
-y_pred = regr.predict(X_test)
 
-# EVALUATION
-rmse = root_mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-print(f"RMSE: {rmse:.3f}  |  R²: {r2:.3f}")
+def main():
 
-plt.scatter(y_test, y_pred, alpha=0.6)
-plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], "r--")
-plt.xlabel("Actual solubility")
-plt.ylabel("Predicted solubility")
-plt.title("Random Forest: Predicted vs Actual")
-plt.savefig("models/randomforest_results_plot.png", dpi=150)
-print("Saved plot to models/results_plot.png")
+    X_train, y_train = prepare_features("data/esol_train.csv")
+    X_test, y_test = prepare_features("data/esol_test.csv")
+
+    model = random_forest(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    # EVALUATION
+    rmse = root_mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    print(f"RMSE: {rmse:.3f}  |  R²: {r2:.3f}")
+
+    plt.scatter(y_test, y_pred, alpha=0.6)
+    plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], "r--")
+
+    plt.title("Predicted vs Actual")
+    plt.savefig("models/results_plot.png", dpi=150)
+    print("Saved plot to models/results_plot.png")
+
+
+main()
