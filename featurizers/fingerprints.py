ad485bf6114260ec274781b0cc5b362b4ab29ad5
Tiphaine H <t.helmer@pi2r.net>
2026-09-13 13:18:58 +0200

    add translation function from smiles to morgan fingerprints

--- featurizers/fingerprints.py
+++ featurizers/fingerprints.py
@@ -1,10 +1,14 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
-
+import logging
 
 gen = AllChem.GetMorganGenerator(radius=2)
 
 
 def smiles_to_fp(smiles):
-    fingerprint = gen.GetFingerprint(smiles)
-    return fingerprint
+    mol = Chem.MolFromSmiles(smiles)
+    if not mol:
+        logging.error("Molecule File contains an error in SMILES")
+        return None
+    fp = gen.GetFingerprint(mol)
+    return fp
